fd89377930c76809d1feaf5bc6b6ec21da88fa3f
Adrian Samaniego <llathander@gmail.com>
2019-07-12 14:59:33 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,18 @@
+18 lines (15 sloc)  295 Bytes
+    
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      },
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
